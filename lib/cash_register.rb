255c0ec593e998ee23f0843acea010a100160dfc
Nicole D <nnardella@gmail.com>
2018-11-19 19:13:48 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -7,14 +7,14 @@ class CashRegister
   def initialize(discount = 0)
     @total = 0
     @discount = discount
-    @items = [] 
+    @items = []
   end
 
   def add_item(title, price, quantity = 1)
     quantity.times do
       @items << title
     end
-    @total += price * quantity
+      @total += price * quantity
   end
 
   def apply_discount
